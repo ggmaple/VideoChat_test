3e299e6457f69451b9858d4549eed8803c4270a0
野原楓 <nohara.kaede.hrlb@gmail.com>
2021-04-09 16:09:31 +0900

    first commit

---
+++ VideoChat.js
@@ -0,0 +1,67 @@
+// import Peer from 'skyway-js';
+
+
+let localStream;
+      
+// カメラ映像取得
+navigator.mediaDevices.getUserMedia({video: {width:640,height:480}, audio: true})
+  .then( stream => {
+  // 成功時にvideo要素にカメラ映像をセットし、再生
+  const VideoElm = document.getElementById('my-video');
+  VideoElm.style = '-webkit-transform: scaleX(-1);';
+  VideoElm.srcObject = stream;
+  VideoElm.play();
+  // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
+  // localStream = stream;
+}).catch( error => {
+  // 失敗時にはエラーログを出力
+  console.error('mediaDevice.getUserMedia() error:', error);
+  return;
+});
+
+// キャプチャしたい canvas 要素を取得
+var canvasElt = document.querySelector('canvas');
+
+// ストリームの取得
+var stream = canvasElt.captureStream(60); // 60 FPS
+localStream = stream;
+
+//Peer作成
+const peer = new Peer({
+  //APIキー
+  key: "57a0f3dc-efe0-49f9-bf2a-8607b50a446f",
+  debug: 3
+});
+
+//PeerID取得
+peer.on('open', () => {
+    console.log("着信しました！");
+    document.getElementById('my-id').textContent = peer.id;
+});
+
+// 発信処理
+document.getElementById('make-call').onclick = () => {
+
+  const theirID = document.getElementById('their-id').value;
+  const mediaConnection = peer.call(theirID, localStream);
+  setEventListener(mediaConnection);
+};
+
+// イベントリスナを設置する関数
+const setEventListener = mediaConnection => {
+  mediaConnection.on('stream', stream => {
+    // video要素にカメラ映像をセットして再生
+    const videoElm = document.getElementById('their-video')
+    videoElm.srcObject = stream;
+    videoElm.play();
+  });
+}
+
+//着信処理
+peer.on('call', mediaConnection => {
+  let Ctx = canvasElt.getContext('2d');
+  Ctx.scale(-1,1);
+  Ctx.translate(-canvas.width, 0);
+  mediaConnection.answer(localStream);
+  setEventListener(mediaConnection);
+});
---
+++ VirtualBack.js
@@ -0,0 +1,64 @@
+// import * as bodyPix from '@tensorflow-models/body-pix';
+// import * as bodyPix from '/node_modules/@tensorflow-models/body-pix/dist/body-pix.js';
+
+const VideoElement = document.getElementById('my-video');
+const canvas = document.getElementById('canvas');
+let canvasCtx = canvas.getContext('2d');
+let handle;
+
+function _canvasUpdate(isOnVideo) {
+  if(isOnVideo){
+    //描画を左右反転
+    canvasCtx.scale(-1,1);
+    canvasCtx.drawImage(VideoElement, 0, 0, -canvas.width, canvas.height);
+    handle = requestAnimationFrame(_canvasUpdate);
+  }else{
+    cancelAnimationFrame(handle);
+  }
+};
+_canvasUpdate(true);
+
+//ぼかし背景
+function segmentBody(bodypixnet) {
+    async function renderFrame() {
+      const segmentation = await bodypixnet.segmentPerson(VideoElement);
+      const backgroundBlurAmount = 3;
+      const edgeBlurAmount = 3;
+      const flipHorizontal = true;
+      bodyPix.drawBokehEffect(
+        canvas, VideoElement, segmentation, backgroundBlurAmount,
+        edgeBlurAmount, flipHorizontal);
+      requestAnimationFrame(renderFrame);
+    }
+    renderFrame();
+  }
+
+async function start() {
+    console.log("バーチャル背景オン");
+    _canvasUpdate(false);
+    let bodypixnet = await bodyPix.load();
+    // segmentBody(bodypixnet);
+    VirtualBack(bodypixnet);
+}
+
+//黒背景
+function VirtualBack(bodypixnet) {
+  async function renderFrame() {
+    const segmentation = await bodypixnet.segmentPerson(VideoElement);
+
+    // Convert the segmentation into a mask to darken the background.
+    const foregroundColor = {r: 0, g: 0, b: 0, a: 0};
+    const backgroundColor = {r: 0, g: 0, b: 0, a: 255};
+    const backgroundDarkeningMask = bodyPix.toMask(segmentation, foregroundColor, backgroundColor);
+
+    const opacity = 1.0;
+    const maskBlurAmount = 1;
+    const flipHorizontal = true;
+    
+    bodyPix.drawMask(
+      canvas, VideoElement, backgroundDarkeningMask, opacity, maskBlurAmount, flipHorizontal);
+
+    requestAnimationFrame(renderFrame);
+  }
+  renderFrame();
+}
